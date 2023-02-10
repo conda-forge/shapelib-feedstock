a6bbca8ac7e12b68d6e9e3d0fd4d0a2bf95335d5
Howard Butler <hobu.inc@gmail.com>
2023-02-10 10:48:00 -0600

    explicit CMAKE_INSTALL_LIBDIR

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,8 +7,11 @@ CXXFLAGS="${CXXFLAGS/-std=c++11/}"
 export CXXFLAGS
 
 mkdir build_shapelib && cd build_shapelib
-cmake ${CMAKE_ARGS} -D CMAKE_INSTALL_PREFIX=$PREFIX \
-      $SRC_DIR
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    $SRC_DIR
 
 make VERBOSE=1
 ctest
