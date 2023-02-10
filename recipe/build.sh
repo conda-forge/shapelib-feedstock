8d568bfa9d668dc1adc8405d814351755d45a280
Howard Butler <hobu.inc@gmail.com>
2023-02-10 14:17:51 -0600

    missing continuation

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,7 @@ mkdir build_shapelib && cd build_shapelib
 cmake ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_LIBDIR="$CONDA_PREFIX/lib" \
-    -DCMAKE_PREFIX_PATH=$PREFIX
+    -DCMAKE_PREFIX_PATH=$PREFIX \
     ..
 
 cmake -LA ..
