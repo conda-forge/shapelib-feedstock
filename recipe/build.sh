b3f4258d7c9674bb2aab8716cab75262f1c3d664
Howard Butler <hobu.inc@gmail.com>
2023-02-10 09:49:54 -0600

    CMAKE_ARGS for proper cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-if [ $(uname) == Darwin ]; then
-  export CC=clang
-  export CXX=clang++
-  export MACOSX_DEPLOYMENT_TARGET="10.9"
-  export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
-  export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
-fi
+set -ex
+
+# strip std settings from conda
+CXXFLAGS="${CXXFLAGS/-std=c++14/}"
+CXXFLAGS="${CXXFLAGS/-std=c++11/}"
+export CXXFLAGS
 
 mkdir build_shapelib && cd build_shapelib
-cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+cmake ${CMAKE_ARGS} -D CMAKE_INSTALL_PREFIX=$PREFIX \
       $SRC_DIR
 
 make VERBOSE=1
